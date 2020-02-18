95da8fac00f6808434e6be15f280341f04e438f4
Christopher Floyd <cfloyd252@gmail.com>
2020-02-18 14:30:04 -0700

    test for genre works

--- index.js
+++ index.js
@@ -9,9 +9,25 @@ app.use(morgan('dev'));
 app.get('/apps', (req,res) => {
   const {sort, genres = ''} = req.query;
 
+  if (sort) {
+    if (!['Rating', 'App'].includes(sort)) {
+      return res
+        .status(400)
+        .send('Sort must be one of rating or app');
+    }
+  }
+  if (genres) {
+    if (!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card']
+      .includes(genres)) {
+      return res
+        .status(400)
+        .send('App Genre should be Action, Puzzle, Strategy, Casual, Arcade, Card');
+    }
+  }
+
   let results = playstoreData
-    .filter(app => 
-      app
+    .filter(playstoreData => 
+      playstoreData
         .Genres
         .toLowerCase()
         .includes(genres.toLowerCase())
